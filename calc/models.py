532590bea359e601a51ad8706121973ebbb487fb
DayChan <daychanhk@gmail.com>
2018-07-13 00:59:54 +0800

    little

--- calc/models.py
+++ calc/models.py
@@ -5,9 +5,11 @@ from django.db import models
 from django.utils.timezone import now
 # Create your models here.
 
+pickname = ["请选择","闯红灯", "压实线", "违章停车"]
 class CureData(models.Model):
+	
 	name = models.CharField('名称', max_length=50)
 	file = models.ImageField('图片', upload_to='photos', blank=True)
 
-def __unicode__(self):
+def __str__(self):
         return self.name
