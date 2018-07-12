bf24091bfa83c32a02a8b1f93c8d23afa062ee70
DayChan <daychanhk@gmail.com>
2018-07-13 01:11:57 +0800

    a

--- calc/test.py
+++ calc/test.py
@@ -19,10 +19,13 @@ def recong(path):
 		model1 = pr.LPR("model/cascade.xml","model/model12.h5","model/ocr_plate_all_gru.h5")
 	result = model1.SimpleRecognizePlateByE2E(grr)
 	#print result
-	best_result = result[0][0]
-	for pstr,confidence,rect in result:
-		if confidence>0.7:	    
-		    print("plate_str",pstr[-6:])
-		    print("plate_confidence",confidence)
-		    #cv2.waitKey(0)
-	return best_result
+	if result:
+		best_result = result[0][0]
+		for pstr,confidence,rect in result:
+			if confidence>0.7:	    
+				print("plate_str",pstr[-6:])
+				print("plate_confidence",confidence)
+				#cv2.waitKey(0)
+		return best_result
+	else:
+		return "no plate"
